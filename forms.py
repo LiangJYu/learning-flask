32c0ab79c7163b904819636559580ef673f24b80
Liang Yu <liangjyu@gmail.com>
2017-09-27 17:13:00 -0700

    added email validation

--- forms.py
+++ forms.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class SignupForm(FlaskForm):
-  first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
-  last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
-  email = StringField('Email', validators=[DataRequired("Please enter your email.")])
-  password = PasswordField('Password', validators=[DataRequired("Please enter your password.")])
+  first_name = StringField('First name',
+                           validators=[DataRequired("Please enter your first name.")])
+  last_name = StringField('Last name',
+                          validators=[DataRequired("Please enter your last name.")])
+  email = StringField('Email',
+                      validators=[DataRequired("Please enter your email."),
+                                  Email("Please enter a valid email.")])
+  password = PasswordField('Password',
+                           validators=[DataRequired("Please enter your password.")])
   submit = SubmitField('Sign up')
