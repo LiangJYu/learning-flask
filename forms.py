d8f2554933c80837333c3b7fe7f5905708de8fd3
Liang Yu <liangjyu@gmail.com>
2017-09-27 17:14:46 -0700

    added minimum password length validation

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(FlaskForm):
   first_name = StringField('First name',
@@ -11,5 +11,6 @@ class SignupForm(FlaskForm):
                       validators=[DataRequired("Please enter your email."),
                                   Email("Please enter a valid email.")])
   password = PasswordField('Password',
-                           validators=[DataRequired("Please enter your password.")])
+                           validators=[DataRequired("Please enter your password."),
+                                       Length(min=6, message="Passwords must be 6 characters or more.")])
   submit = SubmitField('Sign up')
