84e2fbfa3011c6ff29271ca850f7bca95869eae5
Liang Yu <liangjyu@gmail.com>
2017-09-29 14:35:09 -0700

    can now add users into database

--- routes.py
+++ routes.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
-from models import db
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+# change user and pass on test - not acceptable - fix after completing course
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:pass@localhost/learningflask'
 db.init_app(app)
 
 app.secret_key = "development-key"
@@ -25,6 +26,12 @@ def signup():
     if form.validate() == False:
       return render_template('signup.html', form=form)
     else:
+      newuser = User(form.first_name.data,
+                     form.last_name.data,
+                     form.email.data,
+                     form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
       return "Success!"
   elif request.method == 'GET':
     return render_template("signup.html", form=form)
